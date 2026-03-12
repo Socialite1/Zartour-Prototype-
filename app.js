7c6d7d812c09534527e6b1e8296df3ef1d109fd4
Socialite1 <tgsekgololo@gmail.com>
2026-03-12 18:18:26 +0200

    Implement activity feed with user actions
    Add activity feed to display user actions and events.

--- app.js
+++ app.js
@@ -35,3 +35,54 @@ document.getElementById("reward").innerText =
 "Quest complete! +10 cultural points"
 
 }
+
+function loadFeed(){
+
+const feed = document.getElementById("activityFeed")
+
+const activities = [
+
+{
+user:"Anna",
+action:"earned Cultural Explorer badge",
+place:"Bakone Village",
+time:"2 min ago"
+},
+
+{
+user:"David",
+action:"checked in",
+place:"Sacred Tree",
+time:"10 min ago"
+},
+
+{
+user:"Lerato",
+action:"completed a quest",
+place:"Venda Drum Experience",
+time:"30 min ago"
+}
+
+]
+
+activities.forEach(a=>{
+
+const card = document.createElement("div")
+
+card.className="feed-card"
+
+card.innerHTML=`
+
+<div class="feed-user">${a.user}</div>
+
+<div class="feed-action">${a.action} at <b>${a.place}</b></div>
+
+<div class="feed-time">${a.time}</div>
+
+`
+
+feed.appendChild(card)
+
+})
+
+}
